79aa0db97b9440032f625f2e9544017750685041
Matt Cadorette <mcadorette@gmail.com>
2018-11-19 16:33:43 -0500

    Initial commit from Atomist

---
+++ src/main/java/rew123/Rew123Application.java
@@ -0,0 +1,12 @@
+package rew123;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Rew123Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Rew123Application.class, args);
+	}
+}
